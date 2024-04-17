9c220a9f9994c26e1461b7bb0eeac6715387034c
dev2506 <dbhavsar2506@gmail.com>
2024-04-17 06:01:02 +0530

    mongodb initialization skipped

--- relay.py
+++ relay.py
@@ -3,7 +3,7 @@ from mongodb import MongoConnection
 import math
 import mailparser
 
-database = MongoConnection().getConnection()
+# database = MongoConnection().getConnection()
 
 class MessageRelay(Relay):
     def __init__(self):
@@ -11,10 +11,10 @@ class MessageRelay(Relay):
     
     def attempt(self, envelope, attempts):
             print(f"-------Envelope Message--------- {envelope.message}")
-            messagesColl = database.get_collection("messages")
-            messagesColl.insert_one({
-                "sender": envelope.sender,
-                "timestamp": math.ceil(envelope.timestamp),
-                "message": envelope.message.decode("utf-8").strip(),
-                "recipients": envelope.recipients
-            })
+            # messagesColl = database.get_collection("messages")
+            # messagesColl.insert_one({
+            #     "sender": envelope.sender,
+            #     "timestamp": math.ceil(envelope.timestamp),
+            #     "message": envelope.message.decode("utf-8").strip(),
+            #     "recipients": envelope.recipients
+            # })
