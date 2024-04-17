eaad96888322778f9cc7dbbe4bb87a35cb0b5454
dev2506 <dbhavsar2506@gmail.com>
2024-04-17 05:57:29 +0530

    tlsCAFile arg removed

--- mongodb.py
+++ mongodb.py
@@ -7,7 +7,7 @@ class MongoConnection:
     mongoUrl = "mongodb+srv://dev-admin:Dev25062000@cluster0.6ukxabv.mongodb.net/?retryWrites=true&w=majority"
     database = None
     def __init__(self):
-        client = MongoClient(self.mongoUrl, tlsCAFile=certifi.where())
+        client = MongoClient(self.mongoUrl)
         self.database = client.get_database("dis_mails_stage")
 
     def getConnection(self): 
