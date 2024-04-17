09e48142c769018cffd76d46d70912a096a70ddb
dev2506 <dbhavsar2506@gmail.com>
2024-04-17 15:37:47 +0530

    changing the port

--- send.py
+++ send.py
@@ -1,6 +1,6 @@
 import smtplib
 
-server = smtplib.SMTP('13.234.38.143', 25)
+server = smtplib.SMTP('13.234.38.143', 587)
 server.sendmail("test@example.com", ["test@foryou.com"], 'Test123')
 
 server.quit()
